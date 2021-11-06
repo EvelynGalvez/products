2141499eb7f5db9acda2add6ea0fac2e15b42308
EvelynGalvez <evelyn.galvez.p@gmail.com>
2021-11-05 23:17:08 -0300

    primer commit

---
+++ database/migrations/2021_10_25_015857_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('code');
+            $table->string('name',200);
+            $table->string('category', 100);
+            $table->string('description', 200)->nullable();
+            $table->integer('amount');
+            $table->integer('price');
+            $table->foreignId('branch_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
