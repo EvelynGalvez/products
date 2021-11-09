452a04638653b9e181c7adc845ea4054c673b7d5
EvelynGalvez <evelyn.galvez.p@gmail.com>
2021-11-09 16:46:08 -0300

    branch controller

--- app/Http/Controllers/Api/BranchController.php
+++ app/Http/Controllers/Api/BranchController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\CreateBranchRequest;
+use App\Http\Requests\Api\IndexBranchRequest;
+use App\Http\Requests\Api\UpdateBranchRequest;
+use App\Models\Branch;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
@@ -12,43 +16,64 @@ class BranchController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(IndexBranchRequest $request)
     {
-        //
+        $input = $request->validated();
+        $rows = Branch::search($input);
+
+        return response()->json(['message' => 'Datos obtenidos', 'data' => $rows]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  CreateBranchRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateBranchRequest $request)
     {
-        //
+        /*$branch = new Branch();
+        $branch->name = $request->input('name');
+        $branch->save();*/
+
+        $input = $request->validated();
+        $branch = Branch::create($input);
+
+        return response()->json(['message' => 'Datos obtenidos por json', 'data' => $branch]); //Arreglo con 2 indices (message y data)
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Branch $branch
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Branch $branch)
     {
-        //
+        return response()->json(['message' => 'Datos obtenidos', 'data' => $branch]);
+
+        /*
+        $branch = Branch::find($id);
+        if($branch){
+            return response()->json(['message' => 'Datos obtenidos', 'data' => $branch]);
+        }
+        return response()->json(['message' => 'El ID no fue encontrado'], '404');
+        */
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  UpdateBranchRequest  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateBranchRequest $request, Branch $branch)
     {
-        //
+        $input = $request->validated();
+        $branch->update($input);
+        $branch->refresh();
+        return response()->json(['message' => 'Datos actualizados correctamente', 'data' => $branch]);
     }
 
     /**
--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Branch extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+
+    public static function search($input)
+    {
+        $branch = new self();
+        if (isset($input['name'])){
+            $branch = $branch->where('name', $input['name']);
+        }
+
+        return $branch->get();
+    }
 }
